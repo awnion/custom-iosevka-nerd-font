572c469856963d2574314fc4bd22421c868d2d57
Sergei Blinov <blinovsv@gmail.com>
2020-06-09 16:36:06 +0300

    add glyphs and download_glyphs.sh

---
+++ download_glyphs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+# set -v
+
+cd glyphs
+
+GLYPHS=$(cat <<-END
+devicons.ttf
+font-awesome-extension.ttf
+font-logos.ttf
+FontAwesome.otf
+materialdesignicons-webfont.ttf
+octicons.ttf
+original-source.otf
+Pomicons.otf
+PowerlineExtraSymbols.otf
+PowerlineSymbols.otf
+Unicode_IEC_symbol_font.otf
+weathericons-regular-webfont.ttf
+END
+)
+
+for f in $GLYPHS
+do
+    curl -sOL "https://github.com/ryanoasis/nerd-fonts/raw/master/src/glyphs/$f" &
+done
+
+wait
