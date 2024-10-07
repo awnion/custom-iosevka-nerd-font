f57eea7172716fc4e9aed7e8548c921c3dcf6a8f
Sergei Blinov <blinovsv@gmail.com>
2024-10-07 23:55:51 +0200

    update nix

--- flake.nix
+++ flake.nix
@@ -9,11 +9,6 @@
       url = "github:be5invis/Iosevka?shallow=1";
       flake = false;
     };
-
-    # font_config = {
-    #   url = "./private-build-plans.toml";
-    #   flake = false;
-    # };
   };
 
   outputs = { self, nixpkgs, flake-utils, font_src, ... }@inputs: flake-utils.lib.eachDefaultSystem (system:
@@ -25,25 +20,27 @@
         default = pkgs.stdenv.mkDerivation {
           name = "afio";
           buildInputs = with pkgs; [
-            qt5.qmake
-            qt5.qtbase
+            # qt5.qmake
+            # qt5.qtbase
             cacert
-            ttfautohint
-            nodejs
-            nodePackages.pnpm
+            ttfautohint-nox
+            nodejs_22
+            # nodePackages.pnpm
           ];
 
           src = font_src;
 
           buildPhase = ''
             cp -v ${./private-build-plans.toml} private-build-plans.toml
-            pnpm i
-            npm run build -- ttf::afio
+            npm i
+            npm run build ttf::afio
           '';
           installPhase = ''
             mkdir -p $out/
+            mkdir -p $out/share
+            cp -r * $out/share/
             ls -la > $out/ls.txt
-            cp -avL dist/*/ttf/* $out
+            # cp -avL dist/*/ttf/* $out
           '';
         };
       };
