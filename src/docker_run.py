ced5385eb60c38a499277fbd3bac405b3fa56bed
Sergei Blinov <blinovsv@gmail.com>
2025-06-19 22:48:39 +0200

    lint + fmt

--- src/docker_run.py
+++ src/docker_run.py
@@ -10,59 +10,62 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 
 def patch_fonts(index, ttf_file):
-    output_dir = environ.get('OUTPUT_DIR', '/output')
-    log_file = f'{output_dir}/patch.{index}.log'
-    command = " ".join([
-        f'python3 font-patcher -q',
-        f'-s',
-        f'-l',
-        f'-c',
-        f'--careful',
-        f'--makegroups -1', # don't rename font
-
-        ## optional
-        # f'--fontawesome',
-        # f'--fontawesomeextension',
-        # f'--fontlinux',
-        # f'--octicons',
-        # f'--powersymbols',
-        # f'--pomicons',
-        # f'--powerline',
-        # f'--powerlineextra',
-        # f'--material',
-        # f'--weather',
-        f'-out "{output_dir}" "{ttf_file}" >>"{log_file}" 2>&1',
-    ])
-    print('Run command:', command)
+    output_dir = environ.get("OUTPUT_DIR", "/output")
+    log_file = f"{output_dir}/patch.{index}.log"
+    command = " ".join(
+        [
+            "python3 font-patcher -q",
+            "-s",
+            "-l",
+            "-c",
+            "--careful",
+            "--makegroups -1",  # don't rename font
+            ## optional
+            # f'--fontawesome',
+            # f'--fontawesomeextension',
+            # f'--fontlinux',
+            # f'--octicons',
+            # f'--powersymbols',
+            # f'--pomicons',
+            # f'--powerline',
+            # f'--powerlineextra',
+            # f'--material',
+            # f'--weather',
+            f'-out "{output_dir}" "{ttf_file}" >>"{log_file}" 2>&1',
+        ]
+    )
+    print("Run command:", command)
     os.system(command)
-    print('Finish command:', command)
+    print("Finish command:", command)
 
 
 def main():
-    build_dir = environ.get('BUILD_DIR', '/build')
-    output_dir = environ.get('OUTPUT_DIR', '/output')
-    font_name = environ.get('FONT_NAME', 'afio')
+    build_dir = environ.get("BUILD_DIR", "/build")
+    output_dir = environ.get("OUTPUT_DIR", "/output")
+    font_name = environ.get("FONT_NAME", "afio")
 
-    ttf_dir = path.join(build_dir, f'iosevka/dist/{font_name}/TTF')
-    ttf_files = glob.glob(f'{ttf_dir}/*.ttf', recursive=True)
+    ttf_dir = path.join(build_dir, f"iosevka/dist/{font_name}/TTF")
+    ttf_files = glob.glob(f"{ttf_dir}/*.ttf", recursive=True)
 
     with Pool(8) as pool:
         pool.starmap(patch_fonts, enumerate(ttf_files))
 
-    today = date.today().strftime('%Y-%m-%d')
-    zip_filename = f'{output_dir}/{font_name}-{today}.zip'
+    today = date.today().strftime("%Y-%m-%d")
+    zip_filename = f"{output_dir}/{font_name}-{today}.zip"
 
-    print('Make archive:', zip_filename)
-    with ZipFile(zip_filename, 'w', compression=ZIP_DEFLATED, compresslevel=9) as archive:
-        for filename in glob.glob(f'{output_dir}/*.ttf'):
-            print(filename, '-->', zip_filename)
+    print("Make archive:", zip_filename)
+    with ZipFile(
+        zip_filename, "w", compression=ZIP_DEFLATED, compresslevel=9
+    ) as archive:
+        for filename in glob.glob(f"{output_dir}/*.ttf"):
+            print(filename, "-->", zip_filename)
             archive.write(filename, path.basename(filename))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print('...')
-        print('KeyboardInterrupt detected')
+        print("...")
+        print("KeyboardInterrupt detected")
         exit(1)
