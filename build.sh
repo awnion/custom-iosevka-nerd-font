a5ee5428a28d686cadaee9e058e6ee56342b95eb
Sergei Blinov <blinovsv@gmail.com>
2026-03-22 18:49:45 +0100

    optimize docker image with multi-stage build

--- build.sh
+++ build.sh
@@ -4,14 +4,28 @@ set -e
 
 export DOCKER_BUILDKIT=1
 
+BUILD_DIR=/build
+FONT_NAME=${FONT_NAME:-afio}
+BUILD_PLAN=${BUILD_PLAN:-private-build-plans.toml}
 OUTPUT_DIR=$(pwd)/_output
 
 rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
+
 docker buildx build \
     --load \
     --iidfile "$OUTPUT_DIR"/iddfile \
     .
+
 IMAGE_ID=$(cat "$OUTPUT_DIR"/iddfile)
-echo "Run docker ..."
-docker run --rm -t -v "$OUTPUT_DIR":/output "$IMAGE_ID"
+
+echo "Building font '$FONT_NAME' using plan '$BUILD_PLAN' ..."
+
+docker run --rm -t \
+    -v "$OUTPUT_DIR":/output \
+    -v "$(pwd)/$BUILD_PLAN":${BUILD_DIR}/iosevka/private-build-plans.toml:ro \
+    "$IMAGE_ID" -c "\
+        cd ${BUILD_DIR}/iosevka && \
+        bun run build -- ttf::${FONT_NAME} && \
+        cd ${BUILD_DIR} && \
+        python3 docker_run.py"
